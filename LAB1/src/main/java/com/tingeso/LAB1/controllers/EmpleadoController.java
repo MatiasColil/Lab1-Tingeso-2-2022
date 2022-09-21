873ccfd7dcb967a9bd484e1aef43c3a9b56b09a9
Matias Colil <matias.colil@usach.cl>
2022-09-21 20:44:44 -0300

    sonarqube config

--- LAB1/src/main/java/com/tingeso/LAB1/controllers/EmpleadoController.java
+++ LAB1/src/main/java/com/tingeso/LAB1/controllers/EmpleadoController.java
@@ -3,7 +3,6 @@ package com.tingeso.LAB1.controllers;
 import com.tingeso.LAB1.services.EmpleadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
