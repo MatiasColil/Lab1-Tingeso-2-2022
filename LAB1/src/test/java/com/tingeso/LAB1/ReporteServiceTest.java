4213d04adc85ef1b0454e286d1ba72ea0891390f
Matias Colil <matias.colil@usach.cl>
2022-09-23 23:07:38 -0300

    coverage reporteservice 3/10

--- LAB1/src/test/java/com/tingeso/LAB1/ReporteServiceTest.java
+++ LAB1/src/test/java/com/tingeso/LAB1/ReporteServiceTest.java
@@ -2,16 +2,38 @@ package com.tingeso.LAB1;
 
 import com.tingeso.LAB1.entities.EmpleadoEntity;
 import com.tingeso.LAB1.services.ReporteService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.time.LocalDate;
 
 
 class ReporteServiceTest {
 
+    @Autowired
     ReporteService reporte = new ReporteService();
 
+    @Autowired
     EmpleadoEntity empleado = new EmpleadoEntity();
+
+
+    @Test
+    void testDescuentoDiasNoTrabajados(){
+        double diasNoTrabajados = reporte.descuentoDiasNoTrabajados(2,800000);
+        double diasNoTrabajados2 = reporte.descuentoDiasNoTrabajados(5,1700000);
+        double diasNoTrabajados3 = reporte.descuentoDiasNoTrabajados(1,1200000);
+        assertEquals(240000,diasNoTrabajados,0.0);
+        assertEquals(1275000,diasNoTrabajados2,0.0);
+        assertEquals(180000,diasNoTrabajados3,0.0);
+    }
+
+    @Test
+    void testCalcularAniosSeervicio(){
+        int anios = 8;
+        double sueldo = 800000;
+        double beneficio = reporte.calculoAniosServicio(anios,sueldo);
+        assertEquals(40000,beneficio,0.0);
+    }
 
     @Test
     void testAnioServicio(){
@@ -22,15 +44,7 @@ class ReporteServiceTest {
         LocalDate b = LocalDate.of(1920, 5,25);
         empleado.setIngresoEmpresa(a);
         empleado.setFechaNacimiento(b);
-
         double numero = reporte.aniosServicio(empleado);
         assertEquals(72,numero,0.0);
     }
-
-    @Test
-    void testDescuentoDiasNoTrabajados(){
-        double diasNoTrabajados = reporte.descuentoDiasNoTrabajados(2,1700000);
-        assertEquals(510000,diasNoTrabajados);
-    }
-
 }
